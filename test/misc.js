ac5f5d4cdca9cb2ae84e7341d1c292a0fc9f1e72
Victor Leach <victor96@mac.com>
2016-07-02 12:16:06 +0100

    Added throwing completion checker to test cases.

--- test/misc.js
+++ test/misc.js
@@ -3,17 +3,16 @@ if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
 	var validate = require('../vladiator.js');
 }
 
+var primaryColor = function(value){
+	return ["blue", "yellow", "red", "green"].indexOf(value.toLowerCase()) > -1;
+}
+
 describe('Misc', function() {
 	
 	it('should pass for custom primary colour checker', function () {
 		
-		var colours = ["blue", "yellow", "red", "green"];
-		var custom = function(value){
-			return colours.indexOf(value.toLowerCase()) > -1;
-		}
-		
-		var test = validate("blue").isString().is(custom);
-		var test2 = validate("orange").isString().is(custom);
+		var test = validate("blue").isString().is(primaryColor);
+		var test2 = validate("orange").isString().is(primaryColor);
 		var expected = true;
 		
 		test.didPass().should.equal(expected);
@@ -24,13 +23,8 @@ describe('Misc', function() {
 	
 	it('should pass for custom primary colour checker notted', function () {
 		
-		var colours = ["blue", "yellow", "red", "green"];
-		var custom = function(value){
-			return colours.indexOf(value.toLowerCase()) > -1;
-		}
-		
-		var test = validate("blue").isString().not().is(custom);
-		var test2 = validate("orange").isString().not().is(custom);
+		var test = validate("blue").isString().not().is(primaryColor);
+		var test2 = validate("orange").isString().not().is(primaryColor);
 		var expected = false;
 		
 		test.didPass().should.equal(expected);
@@ -41,19 +35,35 @@ describe('Misc', function() {
 	
 	it('should pass for custom primary colour checker using multiple values', function () {
 		
-		var colours = ["blue", "yellow", "red", "green"];
-		var custom = function(value){
-			return colours.indexOf(value.toLowerCase()) > -1;
-		}
-		
-		var test = validate("blue").isString().is(custom).and("yellow").isString().is(custom).and("green").isString().is(custom);
-		var test2 = validate("blue").isString().is(custom).and("orange").isString().is(custom).and("green").isString().is(custom);
+		var test = validate("blue").isString().is(primaryColor).and("yellow").isString().is(primaryColor).and("green").isString().is(primaryColor);
+		var test2 = validate("blue").isString().is(primaryColor).and("orange").isString().is(primaryColor).and("green").isString().is(primaryColor);
 		var expected = true;
 		
 		test.didPass().should.equal(expected);
 		test.didFail().should.not.equal(expected);
 		test2.didPass().should.not.equal(expected);
 		test2.didFail().should.equal(expected);
+	});
+	
+	it('should not throw for custom primary colour checker', function (done) {
+		try{
+			validate("blue").isString().is(primaryColor).throws("ERROR");
+		}catch(error){
+			error.should.be.null;
+			done(new Error('Threw when validation passed'));
+		}
+		done();
+	});
+	
+	it('should throw for custom primary colour checker', function (done) {
+		try{
+			validate("orange").isString().is(primaryColor).throws("ERROR");
+		}catch(error){
+			error.should.equal("ERROR");
+			done();
+			return;
+		}
+		done(new Error('Did not throw when validation failed'));
 	});
 	
 });
